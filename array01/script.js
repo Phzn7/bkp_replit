4d286939bc4cf509091137d10557dd0c6f8f3cc8
Pedrinho <01ferreira.pedro@gmail.com>
2024-10-17 11:32:42 -0300

    /array01

---
+++ array01/script.js
@@ -0,0 +1,13 @@
+// VARIAVEIS UNITARIAS 
+const nome = 'nino'
+const nome2 = 'kabrinha'
+const nome3 = 'peçinha'
+
+
+
+// VARIAVEIS DE ARRAY
+const nomes = ['nino', 'kabrinha', 'peçinha']
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = 'buzeira'
+console.log(nomes[10])
