48d83694fb024dde7508eaa9bc8fa2cdb3a85059
Pedrinho <01ferreira.pedro@gmail.com>
2024-10-17 11:33:57 -0300

    /rEVISAO

---
+++ revisao-2TRI/VARIAVEIS/script.js
@@ -0,0 +1,9 @@
+// # ID ou indentificador
+// . Class ou classe CSS
+const comidas = document.querySelector('.comidas');
+
+const comida1 = prompt('digite sua comida n°1 ')
+
+comidas.innerText = comida1;
+
+alert("Vocé gosta de: " + comida1)
